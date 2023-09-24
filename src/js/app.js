1f85672e5f88e8feb5dddec2431d53d4ff5a9358
Kynhogat <khaled270106@gmail.com>
2023-09-24 10:37:32 +0200

    new things were added, done with hero

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+
+$('.mu-skill-progress-bar').appear(function() {
+
+    $('.mu-html5-bar').LineProgressbar({
+       percentage: 70,
+       triggerOnce: true
+   });
+
+   $('.mu-css-bar').LineProgressbar({
+       percentage: 50,
+       triggerOnce: true
+   });
+
+   $('.mu-photoshop-bar').LineProgressbar({
+       percentage: 30,
+       triggerOnce: true
+   });
+
+   $('.mu-wordpress-bar').LineProgressbar({
+       percentage: 20,
+       triggerOnce: true
+   });
+
+   $('.mu-jquery-bar').LineProgressbar({
+       percentage: 10,
+       triggerOnce: true
+   });
+
+});
