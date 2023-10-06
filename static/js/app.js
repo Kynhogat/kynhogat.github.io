938d30b412c3752e4207d0746dc59302cf354189
Kynhogat <khaled270106@gmail.com>
2023-10-06 05:29:41 +0200

    new nav-bar, new footer

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,7 @@
+const hamburger = document.getElementById('hamburgerIcon');
+const nav = document.querySelector('.nav-2');
 
-$('.mu-skill-progress-bar').appear(function() {
-
-    $('.mu-html5-bar').LineProgressbar({
-       percentage: 70,
-       triggerOnce: true
-   });
-
-   $('.mu-css-bar').LineProgressbar({
-       percentage: 50,
-       triggerOnce: true
-   });
-
-   $('.mu-photoshop-bar').LineProgressbar({
-       percentage: 30,
-       triggerOnce: true
-   });
-
-   $('.mu-wordpress-bar').LineProgressbar({
-       percentage: 20,
-       triggerOnce: true
-   });
-
-   $('.mu-jquery-bar').LineProgressbar({
-       percentage: 10,
-       triggerOnce: true
-   });
-
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    hamburger.classList.toggle('active');
 });
